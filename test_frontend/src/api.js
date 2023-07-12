28e9e46197ac2513ba68ad135ceb7e8f30aefae5
topdeveloper917 <jamessmith0917topdev@gmail.com>
2023-07-11 20:51:30 -0700

    configuration change

--- test_frontend/src/api.js
+++ test_frontend/src/api.js
@@ -1,10 +1,8 @@
 import axios from 'axios';
-import dotenv from 'dotenv';
-dotenv.config();
 
 const access = localStorage.getItem('access_token');
 const api = axios.create({
-  baseURL: process.env.REACT_APP_API_URL | 'http://localhost:8000/api/',
+  baseURL: 'http://localhost:8000/api/',
   headers: {
     'Content-Type': 'application/json',
     Authorization: access ? `Bearer ${access}` : '',
